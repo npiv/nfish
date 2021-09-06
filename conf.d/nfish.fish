31fdda118460ed29f9ab8da9ce69e2cb37da39db
Nick Verlinde <nick.verlinde@gmail.com>
2021-09-06 06:54:32 +0200

    initial

---
+++ conf.d/nfish.fish
@@ -0,0 +1,16 @@
+alias gs="git status -sb"
+alias cls="clear"
+alias gla="git log --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(cyan)%an %Creset: %s %Cgreen(%cr)'"
+alias gl="gla -20"
+alias gll="gla --graph"
+alias grep="grep --color"
+alias vi=vim
+alias gpom="git push origin master"
+alias standup_report="gl --author=Nick --since=Yesterday"
+
+function c
+    cd ~/code/$argv
+end      
+complete --no-files --exclusive --command c --arguments "(pushd ~/code && __fish_complete_directories && popd)"
+
+set --global tide_right_prompt_items status cmd_duration context jobs virtual_env kubectl vi_mode time
